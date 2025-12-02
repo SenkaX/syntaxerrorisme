eefb95c00f0eb210fab1c984fe0f89e7800eaa34
Senka <xsenka7@gmail.com>
2025-12-02 14:57:33 +0100

    Add sitemap.xml for Google SEO

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,38 @@ Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 
+// Sitemap pour Google
+Route::get('/sitemap.xml', function () {
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    
+    // Toutes vos pages
+    $pages = [
+        ['url' => url('/'), 'priority' => '1.0'],
+        ['url' => route('about'), 'priority' => '0.8'],
+        ['url' => route('guides'), 'priority' => '0.8'],
+        ['url' => route('tutorials'), 'priority' => '0.8'],
+        ['url' => route('tools'), 'priority' => '0.8'],
+        ['url' => route('glossary'), 'priority' => '0.8'],
+        ['url' => route('faq'), 'priority' => '0.7'],
+        ['url' => route('contact'), 'priority' => '0.6'],
+        ['url' => route('blog.index'), 'priority' => '0.9'],
+    ];
+    
+    foreach ($pages as $page) {
+        $sitemap .= '<url>';
+        $sitemap .= '<loc>' . $page['url'] . '</loc>';
+        $sitemap .= '<lastmod>' . date('Y-m-d') . '</lastmod>';
+        $sitemap .= '<changefreq>weekly</changefreq>';
+        $sitemap .= '<priority>' . $page['priority'] . '</priority>';
+        $sitemap .= '</url>';
+    }
+    
+    $sitemap .= '</urlset>';
+    
+    return response($sitemap)->header('Content-Type', 'application/xml');
+});
+
 // Sitemap XML
 Route::get('/sitemap.xml', [HomeController::class, 'sitemap'])->name('sitemap');
 
